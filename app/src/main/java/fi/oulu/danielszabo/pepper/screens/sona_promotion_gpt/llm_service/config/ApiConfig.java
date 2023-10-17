a630d8c149ad8fd6b3127d34766c5783c433db7d
szabodanika <d@szab.eu>
2023-10-18 00:05:21 +0300

    Update ApiConfig.java

--- app/src/main/java/fi/oulu/danielszabo/pepper/screens/sona_promotion_gpt/llm_service/config/ApiConfig.java
+++ app/src/main/java/fi/oulu/danielszabo/pepper/screens/sona_promotion_gpt/llm_service/config/ApiConfig.java
@@ -1,7 +1,7 @@
 package fi.oulu.danielszabo.pepper.screens.sona_promotion_gpt.llm_service.config;
 
 public class ApiConfig {
-  public static final String API_TOKEN = "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";
+  public static final String API_TOKEN = "add your own token here";
 
   static {
     assert !API_TOKEN.equals("add your own token here") : "You must specify your OpenAI API token here!";
